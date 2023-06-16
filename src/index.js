45537e7d980235de2663bdefd0b743b8ff9e5e94
Sean Chatman <sean_chatman@intuit.com>
2023-06-15 18:48:44 -0700

    First commit.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/**
+=========================================================
+* Material Dashboard 2 PRO React - v2.2.0
+=========================================================
 
-ReactDOM.render(<App />, document.getElementById('root'));
+* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "App";
+
+// Material Dashboard 2 PRO React Context Provider
+import { MaterialUIControllerProvider } from "context";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <MaterialUIControllerProvider>
+      <App />
+    </MaterialUIControllerProvider>
+  </BrowserRouter>
+);
